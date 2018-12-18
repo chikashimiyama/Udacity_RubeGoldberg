c202d8f537330d1cd566c531fa4f0febf532ed2d
Chikashi Miyama <chikashimiyama@gmail.com>
2018-12-18 03:57:38 +0100

    add essential materials

---
+++ Assets/Scripts/BallReset.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class BallReset : MonoBehaviour
+{
+
+    private Rigidbody rigidbody_;
+
+    private void Start()
+    {
+        rigidbody_ = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (!col.CompareTag("Ground")) return;
+        
+        rigidbody_.isKinematic = true;
+        transform.position = new Vector3(0.533f, 0.861f, 0.48f);
+        rigidbody_.isKinematic = false;
+    }
+}
