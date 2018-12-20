cc7ba576b2d022ffe6eb166e10f3c8217bdb704a
Chikashi Miyama <chikashimiyama@gmail.com>
2018-12-20 21:19:36 +0900

    Ball spawner

---
+++ Assets/Scripts/behaviour/BallSpawnerBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class BallSpawnerBehaviour : MonoBehaviour
+{
+    [SerializeField] private float threshold_;
+    [SerializeField] private GameObject demoBall_;
+
+    private Rigidbody rbody_;
+    private float sum_;
+
+    void Start()
+    {
+        rbody_ = demoBall_.GetComponent<Rigidbody>();
+    }
+    
+    void Update()
+    {
+        sum_ += Time.deltaTime;
+        if (sum_ > threshold_)
+        {
+            Respawn();
+            sum_ = 0f;
+        }
+    }
+
+    private void Respawn()
+    {
+        rbody_.ResetInertiaTensor();
+        rbody_.ResetCenterOfMass();
+        rbody_.velocity = Vector3.zero;
+        rbody_.position = gameObject.transform.position;
+    }
+
+}
---
+++ Assets/Scripts/behaviour/WindBehaviour.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class WindBehaviour : MonoBehaviour
+{
+	private void OnTriggerStay(Collider col)
+	{
+		if(col.attachedRigidbody)
+			col.attachedRigidbody.AddForce(-transform.forward.x, 0, -transform.forward.z);
+	}
+}
